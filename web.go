c720cb1d7f301bd10981f720256594a92149aa19
Johan Östling <johan.f.ostling@gmail.com>
2014-04-18 23:00:36 +0200

    First working version

--- web.go
+++ web.go
@@ -1,20 +1,67 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "os"
+	"bufio"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+var (
+	animals    = readFile("words/animals.txt")
+	adjectives = readFile("words/adjectives.txt")
 )
 
 func main() {
-    http.HandleFunc("/", hello)
-    fmt.Println("listening...")
-    err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
-    if err != nil {
-      panic(err)
-    }
+	rand.Seed(time.Now().UnixNano())
+	http.HandleFunc("/", phraser)
+	log.Println("listening...")
+
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	if err != nil {
+		panic(err)
+	}
 }
 
-func hello(res http.ResponseWriter, req *http.Request) {
-    fmt.Fprintln(res, "hello, world")
+func phraser(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(res, adjectives.OneOf()+" "+animals.OneOf())
+}
+
+type Dictionary []string
+
+func (d Dictionary) Contains(s string) bool {
+	for _, value := range d {
+		if value == s {
+			return true
+		}
+	}
+	return false
+}
+
+func (d Dictionary) OneOf() string {
+	return d[rand.Intn(len(d))]
+}
+
+func readFile(filename string) Dictionary {
+	result := Dictionary{}
+
+	file, error := os.Open(filename)
+	if error != nil {
+		log.Fatal(error)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := strings.ToLower(scanner.Text())
+		if len(line) > 1 && !result.Contains(line) {
+			result = append(result, line)
+		}
+	}
+	return result
 }
